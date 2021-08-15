87b5c7530ad305f1f40efc4a679022d2cbfeca63
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-08-15 13:18:18 +0900

    Use node-semver

--- mod.ts
+++ mod.ts
@@ -1,21 +1,26 @@
 /// <reference path="./typings.d.ts" />
 
+import semver from "https://esm.sh/semver@7";
+
 export async function redirect(url: string): Promise<string> {
   const _url = new URL(url);
-  const tagHint = _url.pathname.match(/@([^/]*)/)?.[1] || "";
+  const tagHint = decodeURIComponent(_url.pathname.match(/@([^/]*)/)?.[1] || "");
   const [, pkg, path] = _url.pathname.match(
     /^\/(?:x\/)?([-_a-zA-Z0-9]+)(?:@[^/]*)?(.*)/,
   )!;
   const request = `https://cdn.deno.land/${pkg}/meta/versions.json`;
   const response = await fetch(request);
   const json = await response.json();
-  const tags = json.versions.filter((tag: string) => tag.match("^" + tagHint));
+  const tags = json.versions.filter((tag: string) => {
+      console.log([tag, tagHint])
+      return semver.satisfies(tag, tagHint);
+  });
   const latestTag = tags[0];
   return `https://deno.land/x/${pkg}${latestTag ? "@" + latestTag : ""}${path}`;
 }
 
 self.addEventListener("fetch", async (event) => {
-  let dest =  "https://github.com/tani/lib.deno.dev"
+  let dest = "https://github.com/tani/lib.deno.dev";
   if (new URL(event.request.url).pathname !== "") {
     dest = await redirect(event.request.url);
   }
