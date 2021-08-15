846e9d9e4739643eb2bc875ffe27802721df2e04
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-08-15 10:31:29 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -16,7 +16,7 @@ export async function redirect(url: string): Promise<string> {
 
 self.addEventListener("fetch", async (event) => {
   let dest =  "https://github.com/tani/lib.deno.dev"
-  if (new URL(event.request.url).pathname !== "") {
+  if (new URL(event.request.url).pathname !== "/") {
     dest = await redirect(event.request.url);
   }
   event.respondWith(Response.redirect(dest, 302));
